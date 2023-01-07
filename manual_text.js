6f545895c87ec2206cc80eea3cb95a79b1bcb8f1
ksm042900 <ksm042900@naver.com>
2023-01-07 14:42:12 +0900

    01/07 food manul commit

---
+++ manual_text.js
@@ -0,0 +1,7 @@
+function getManual(){
+    manual = [      
+        ["This food is gimbap. Kimbap is one of the foods that Koreans enjoy eating, and it is characterized by being wrapped in seaweed."],
+        ["b"],
+
+      ];
+}
