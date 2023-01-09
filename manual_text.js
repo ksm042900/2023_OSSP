a62cfc78250de8ac6c1fc5d434cc5ca20dbf2bc2
ksm042900 <90131881+ksm042900@users.noreply.github.com>
2023-01-09 12:55:04 +0900

    manual text add commit

--- manual_text.js
+++ manual_text.js
@@ -1,7 +1,71 @@
 function getManual(){
     manual = [      
-        ["This food is gimbap. Kimbap is one of the foods that Koreans enjoy eating, and it is characterized by being wrapped in seaweed."],
-        ["b"],
+        //index[0] 갈비구이
+        ["This food is 'galbigui' or 'galbi'. It is a food made using the ribs of a cow or pig."],
+        //index[1] 갈치구이
+        ["This food is 'galchigui'. Season the cut hairtail with salt and grill it on a grill or pan. If you sprinkle flour on the trimmed hairtail or coat it with oil and bake it in a pan, the fishy taste is removed and the taste is light."],
+        //index[2] 고등어구이
+        ["This food is 'godeungeogui'. It is a food that is grilled on a grill or pan by cutting mackerel with thick salt in half. It is sometimes sprinkled with salt in advance and dried thickly, or salted and baked."],
+        //index[3] 곱창구이
+        ["This food is 'gopchanggui'. It is a food that is prepared so that the intestines of cattle do not smell, grilled over fire, dipped in sauce, or grilled with various spices."],
+        //index[4] 닭갈비
+        ["This food is 'dakgalbi'. Chicken pieces marinated in a gochujang, and stir-fried in a flat grill pan with cabbage, sweet potato, carrots, and tteok (rice cakes). A local dish of Chuncheon city in Gangwon Province."],
+        //index[5] 더덕구이
+        ["This food is 'deodeokgui'. Deodeok (bonnet bellflower root) brushed with sweet and spicy gochujang and grilled. The slightly bitter deodeok root has health benefits similar to ginseng." ],
+        //index[6] 떡갈비
+        ["This food is 'tteokgalbi'. Minced beef rib meat seasoned with garlic and soy sauce, molded around the bone and chargrilled while brushing with a soy sauce mixture. It is characterized by its soft yet chewy texture."],
+        //index[7] 불고기
+        ["This food is 'bulgogi'. Bulgogi is one of the Korean foods that is eaten by cutting meat from various animals thinly, marinating it in seasonings, and grilling it over fire. It is also one of the world's favorite Korean foods because it has a savory and salty taste rather than a strong taste unique to Korean food that can be divided into likes and dislikes in some cases."],
+        //index[8] 장어구이
+        ["This food is 'jangeogui'. Cleaned eel are seasoned with salt, soy sauce, gochujang, and others to taste, and then grilled. Eel is rich in vitamins A, B, and C, which fight aging and keep the skin healthy and youthful."],
+        //index[9] 조개구이
+        ["This food is 'jogaegui'. For this dish, clams are soaked in salt water to purge the sand from them and carefully cleaned before being grilled and served with a vinegar-based gochujang. These large clams are harvested from a clean beach and are very tasty."],
+        //index[10] 황태구이
+        ["This food is 'hwangtaegui'. Winter air-dried pollack (hwangtae) filleted, deboned, and brushed with a gochujang and grilled."],
+        //index[11] 계란국
+        ["This food is 'dalgyalguk'. This is an anchovy-based soup that is boiled with beaten eggs and seasoned with soy sauce and salt. Chopped green onion is added at the last moment. Thanks to its simple ingredients, it is one of the easiest and quickest Korean dishes to make."],
+        //index[12] 미역국
+        ["This food is 'miyeokguk'. This Miyeok (brown seaweed) simmered in clear beef, mussel, or anchovy broth. Miyeok is rich in iodine and calcium and is known to be particularly beneficial for postpartum mothers, which is one reason Koreans eat Miyeokguk on birthdays as well as for other meals."],
+        //index[13] 북엇국
+        ["This food is 'Bugeotguk'. A clear fish soup made with dried pollack. The dried flesh is shredded and sautéed in sesame oil before water is added. A beaten egg is sometimes dropped into the boiling soup. It is reputed to be one of the best hangover remedies, as it helps remove alcohol from the body."],
+        //index[14] 시래기국
+        ["This food is 'Siraegiguk'. "],
+        //index[15] 육개장
+        ["This food is 'yukgaejang'. A soup made of beef brisket, radishchive, taro stems, and fiddleheads, and seasoned with red chili powder to make it spicy."],
+        //index[16] 콩나물국
+        ["This food is 'kongnamulguk'. This clear soup is made with bean sprouts boiled in water. Seasoned only with salt, Kongnamulguk (bean sprout soup) is known to be effective for easing a cold or a hangover. Add red chili powder if you prefer a spicy soup. Warning: To avoid a fishy smell, do not lift the lid of the pot until the bean sprouts are completely cooked."],
+        //index[17] 파김치
+        ["This food is 'Pakimachi'. This dish features kimchi with medium-sized chives and scallions mixed with red pepper and salted and fermented anchovies. In order to eliminate the strong, pungent taste that is unique to green onion, Pakimchi needs to be fermented for quite a long time."],
+        //index[18] 깍두기
+        ["This food is 'kkakdugi'. Cubed, bite-sized radish seasoned with red chili powder and salted and fermented shrimp. Kkakdugi is a required side dish for seolleongtang (ox bone soup)."],
+        //index[19] 오이소박이
+        ["This food is 'oisobagi'. Cucumbers cut lengthwise, slit open, and stuffed with a mixture of chives, garlic, and red chili powder. Makes a crunchy and refreshing summer kimchi."],
+        //index[20] 열무김치
+        ["This food is 'yeolmukimchi'. Young summer radish is characterized by its short root and thick, abundant green leaves. Yeolmukimchi (young summer radish kimchi) is made without liquid by adding salted shrimp or with liquid by adding rice paste. It is often eaten in the summer with cold noodles or rice mixed with gochujang."],
+        //index[21] 백김치
+        ["This food is 'baekkimchi'. Salted Napa cabbage stuffed with a mixture of white radish, Asian pear, Korean parsley, chestnuts, and salted fish. Extra water is poured onto the kimchi. The absence of red chili powder gives this kimchi a mild and refreshing taste."],
+        //index[22] 무생채
+        ["This food is 'musaengchae'. A sweet and tangy dish of julienned white radish seasoned with vinegar, sugar, and red chili powder. Chili powder may be omitted for a milder version."],
+        //index[23] 총각김치
+        ["This food is 'chonggakkimchi'. Whole or half-split young white radishes, with the leaves on, salted and mixed with red chili powder and seasonings."],
+        //index[24]
+        [""],
 
+
+
+
+
+        
+
+
+
+
+
+      
+      
+      
+      
+      
+      
       ];
 }
